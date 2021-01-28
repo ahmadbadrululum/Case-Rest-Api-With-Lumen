48cfa091183de19f929e29c84007f1ff12d67996
Ahmad  Badrul Ulum <ahmadbadrul635@gmail.com>
2021-01-28 07:58:50 +0700

    function response error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,11 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function responseError($message){
+        $responseError = [
+            'success'=> false,
+            'message' => $message
+        ];
+        return response()->json($responseError, 500);
+    }    
 }
